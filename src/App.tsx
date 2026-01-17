38425eb5fb33dc41a5cbe94a63e08fe30f8b6409
rogerio bispo <rogerio_pd@yahoo.com.br>
2026-01-17 20:11:01 -0300

    ˜initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>React avanaçado</h1>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+
+export default defineConfig({
+  plugins: [react(), vanillaExtractPlugin()],
+})
