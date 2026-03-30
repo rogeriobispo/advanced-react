4f595d2e00179aaa5313e53aa53da761b0abbc05
Rogério Bispo <rogerio_pd@yahoo.com.br>
2026-03-30 07:16:52 -0300

    feat: versão debounce implementado

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import SearchInput from "./component/SearchInput";
 
 function App() {
--- src/component/SearchInput/index.tsx
+++ src/component/SearchInput/index.tsx
@@ -4,24 +4,40 @@ const SearchInput = () => {
   const [query, setQuery] = useState("");
 
   useEffect(() => {
-    if (query.length > 0) {
-      console.log(`Buscando por: "${query}"...`);
+    if (!query) return;
+
+    const timer = setTimeout(() => {
+      console.log(`Buscando por: "${query}"... (Processamento após delay)`);
 
       const startTime = performance.now();
       while (performance.now() - startTime < 150) { }
+    }, 500);
+
+    return () => {
+      console.log('limpeza do timer anterior');
+      clearTimeout(timer);
     }
   }, [query]);
 
+  useEffect(() => {
+    console.log("App montado");
+
+    return () => {
+      console.log("App desmontado");
+    }
+  }, []);
+
   return (
     <div style={{ padding: "20px", border: "1px solid #ccc", marginTop: "20px" }}>
-      <h3>Exemplo de Input sem Debounce</h3>
+      <h3>Exemplo de Input com Debounce</h3>
       <input
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        placeholder="Digite rápido para sentir o lag..."
+        placeholder="Digite rápido... o debounce vai agir!"
         style={{ padding: "8px", width: "300px" }}
       />
+      <p>Status: {query ? "Aguardando pausa..." : "Aguardando input..."}</p>
     </div>
   );
 };
